505a658c53ce7edb3442d5301488d51740c61e37
EvgenyBagdasaryan <66093679+EvgenyBagdasaryan@users.noreply.github.com>
2023-05-11 14:03:42 +0300

    task-4 velRess на 11 java не взлетел

---
+++ velRessService11/src/main/kotlin/ru/eugene/velressservice11/VelRessService11Application.kt
@@ -0,0 +1,11 @@
+package ru.eugene.velressservice11
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VelRessService11Application
+
+fun main(args: Array<String>) {
+    runApplication<VelRessService11Application>(*args)
+}
---
+++ velRessService11/src/test/kotlin/ru/eugene/velressservice11/VelRessService11ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.eugene.velressservice11
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VelRessService11ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
