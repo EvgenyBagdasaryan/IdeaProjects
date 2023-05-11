734e0e8383547258d0122f3b2484032accf934f3
EvgenyBagdasaryan <66093679+EvgenyBagdasaryan@users.noreply.github.com>
2023-05-11 13:22:22 +0300

    task-3 общий сервис

---
+++ velRessService/src/main/kotlin/ru/eugene/velressservice/VelRessServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.eugene.velressservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VelRessServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<VelRessServiceApplication>(*args)
+}
