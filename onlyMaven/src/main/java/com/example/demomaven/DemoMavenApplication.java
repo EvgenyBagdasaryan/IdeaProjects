1aaff305bbada02df0c061610937f97a55ea7fb5
EvgenyBagdasaryan <66093679+EvgenyBagdasaryan@users.noreply.github.com>
2023-05-12 03:01:11 +0300

    пробы

--- onlyMaven/src/main/java/com/example/demomaven/DemoMavenApplication.java
+++ onlyMaven/src/main/java/com/example/demomaven/DemoMavenApplication.java
@@ -2,9 +2,8 @@ package com.example.demomaven;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.MappedByteBuffer;
+import java.util.*;
 
 public class DemoMavenApplication {
 
@@ -44,11 +43,53 @@ public class DemoMavenApplication {
 
         System.out.println(kjk.model);
 
+        Map<String, String> map = new HashMap<>();
+
+        int[] massiv = new int[10];
+
+        List<String> ll = new LinkedList<>();
+
+        for (int i = 0; i < 1000; i++){
+
+        }
+
+
     }
 
     public static void loadFile() throws NullPointerException, FileNotFoundException {
 
         File newFile = new File("dss");
         Scanner sc =  new Scanner(newFile);
+    }
+}
+
+class Season {
+    int number;
+    String name;
+
+    public Season(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Season season = (Season) o;
+        return number == season.number && Objects.equals(name, season.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, name);
     }
 }
